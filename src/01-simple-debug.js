f60fb3ae465e4ed61795100f3dcae0c8d4d03eda
unknown <akaidylan81@gmail.com>
2025-06-27 10:56:25 +0100

    initial commit

--- src/01-simple-debug.js
+++ src/01-simple-debug.js
@@ -1,6 +1,13 @@
-console.log('Simple debugging example running.')
-debugger
+console.log('Simple debugging example running.');
+debugger;
 
-let x = 99
-debugger
-console.log(x)
+let x = 99;
+console.log(x);
+
+console.log(numPointsScored("Ben Gordon"));        
+console.log(shoeSize("Brook Lopez"));              
+console.log(teamColors("Brooklyn Nets"));          
+console.log(teamNames());                          
+console.log(playerNumbers("Charlotte Hornets"));   
+console.log(playerStats("Alan Anderson"));         
+console.log(bigShoeRebounds());                    
